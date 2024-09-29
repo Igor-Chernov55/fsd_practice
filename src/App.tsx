4fb2b97f57e1efe8a655ab018d6d26f4d16f9bc7
Igor-Chernov55 <igorek_chernov_1998@mail.ru>
2024-09-29 23:44:44 +0300

    added styled components, routing with async chunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage";
+import {MainPageAsync} from "./pages/MainPage";
+import {Suspense} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <Link to={'/'}>Главная</Link>
+        <Link to={'/about'}>О сайте</Link>
+
+        <Suspense fallback={<p>...Loading</p>}>
+            <Routes>
+            <Route path={'/about'} element={<AboutPageAsync/>}/>
+            <Route path={'/'} element={<MainPageAsync/>}/>
+        </Routes>
+        </Suspense>
+    </div>
   )
 }
 
