08492b7ea2cae429e37975407e4f3267e4798e81
Igor <ichernov>
2024-10-08 16:25:07 +0300

    change tsconfig

--- src/app/providers/theme/ui/ContextToggleTheme.tsx
+++ src/app/providers/theme/ui/ContextToggleTheme.tsx
@@ -1,14 +1,14 @@
 import {ToggleThemeProvider} from "../lib/ToggleThemeProvider.ts";
-import {FC, useState} from "react";
+import {ReactNode, useState} from "react";
 import {Theme} from "../../../App.tsx";
 
-const defaultTheme = localStorage.getItem('theme') || 'light'
+const defaultTheme = localStorage.getItem('theme') as Theme || Theme.LIGHT
 
-export const ContextToggleTheme:FC = ({children}) => {
-    const [theme, setTheme] = useState(defaultTheme)
+export const ContextToggleTheme= ({children}: {children: ReactNode}) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme)
 
     return (
-        <ToggleThemeProvider.Provider value={{theme:theme, setTheme:setTheme}}>
+        <ToggleThemeProvider.Provider value={{theme: theme, setTheme: setTheme}}>
             {children}
         </ToggleThemeProvider.Provider>
     );
