92d90d3cadcd99b0b3f01473a4d74799e7395317
Igor-Chernov55 <igorek_chernov_1998@mail.ru>
2024-11-04 23:26:53 +0300

    change jestr config

--- src/pages/AboutPage/ui/AboutPage.test.tsx
+++ src/pages/AboutPage/ui/AboutPage.test.tsx
@@ -1,9 +1,8 @@
-import {render, screen} from "@testing-library/react";
-import {AboutPage} from "../index";
+
 
 describe('AboutPage', () => {
     it('should render AboutPage', () => {
-      const {getByText} = render(<AboutPage />);
+
 
       expect(screen.getByText('df'))
     })
